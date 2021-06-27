7691c56829bd8573a2f96a6cfe967d6e2be329c8
kyungsun choi <kschoi0308@gmail.com>
2021-06-27 23:51:47 +0900

    프로젝트 세팅

---
+++ webpack.dev.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./public"),
+    hot: true,
+    port: 3000,
+  },
+};
